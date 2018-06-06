53bd5080eae9b10e0a8975bd627901412b8d71c2
Isaac Kwon <isaac-kwon@hotmail.com>
2018-06-06 15:08:23 -0700

    refactor

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use((err, req, res, next) => {
 
 if (['production', 'ci'].includes(process.env.NODE_ENV)) {
   app.use(express.static('client/build'));
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
